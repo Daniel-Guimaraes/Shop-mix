e99affcd38fb500c41b04c435007e3b6d0a36c02
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-10-21 18:18:16 -0300

    Criação da estilização global do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { globalCss } from '.'
+
+export const GlobalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+  },
+
+  body: {
+    'webkit-font-smoothing': 'antialiased',
+    backgroundColor: '$background',
+    color: '$text',
+  },
+
+  'body, button, textarea, input': {
+    fontFamily: 'Roboto, sans-serif',
+    fontWeight: 400,
+  },
+})
