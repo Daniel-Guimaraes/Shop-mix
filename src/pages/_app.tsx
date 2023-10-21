35290d9be1ae1a3657afaea1e7521bd436608e09
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-10-21 18:54:04 -0300

    criação do cabeçalho padrão da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import { GlobalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from '@/assets/logo.svg'
+
+import * as S from '@/styles/pages/app'
+
 GlobalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <S.Container>
+      <S.Header>
+        <img src={logoImg.src} alt="" />
+      </S.Header>
+
+      <Component {...pageProps} />
+    </S.Container>
+  )
 }
