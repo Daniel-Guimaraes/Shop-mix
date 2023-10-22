5d6499d63178dde03524d8f7106ee3ab48586af7
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-10-22 09:40:07 -0300

    otimizei a logo usando o componente do 'Image' do NEXT

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import { GlobalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 
 import logoImg from '@/assets/logo.svg'
 
+import { GlobalStyles } from '@/styles/global'
 import * as S from '@/styles/pages/app'
 
 GlobalStyles()
@@ -11,7 +12,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <S.Container>
       <S.Header>
-        <img src={logoImg.src} alt="" />
+        <Image
+          src={logoImg}
+          alt="logo"
+          width={logoImg.width}
+          height={logoImg.height}
+        />
       </S.Header>
 
       <Component {...pageProps} />
