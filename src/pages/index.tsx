d7babc44a2c9d59d2efcf2e13644a9c6afbf95e3
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-10-23 07:29:50 -0300

    implementei a lib 'keen-slider' para criar um carrossel dos meus produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+
 import tShirt1 from '@/assets/shirts/1.svg'
 import tShirt2 from '@/assets/shirts/2.svg'
 import tShirt3 from '@/assets/shirts/3.svg'
@@ -8,9 +11,25 @@ import tShirt4 from '@/assets/shirts/4.svg'
 import * as S from '@/styles/pages/home'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 1,
+      spacing: 42,
+    },
+
+    breakpoints: {
+      '(min-width: 760px)': {
+        slides: {
+          perView: 2,
+          spacing: 42,
+        },
+      },
+    },
+  })
+
   return (
-    <S.HomeContainer>
-      <section>
+    <S.HomeContainer ref={sliderRef} className="keen-slider">
+      <section className="keen-slider__slide">
         <S.Product href="#">
           <Image src={tShirt1} alt="Camiseta 1" width={520} height={480} />
           <S.ProductFooter>
@@ -20,7 +39,7 @@ export default function Home() {
         </S.Product>
       </section>
 
-      <section>
+      <section className="keen-slider__slide">
         <S.Product href="#">
           <Image src={tShirt2} alt="Camiseta 2" width={520} height={480} />
           <S.ProductFooter>
@@ -30,7 +49,7 @@ export default function Home() {
         </S.Product>
       </section>
 
-      <section>
+      <section className="keen-slider__slide">
         <S.Product href="#">
           <Image src={tShirt3} alt="Camiseta 3" width={520} height={480} />
           <S.ProductFooter>
@@ -40,7 +59,7 @@ export default function Home() {
         </S.Product>
       </section>
 
-      <section>
+      <section className="keen-slider__slide">
         <S.Product href="#">
           <Image src={tShirt4} alt="Camiseta 4" width={520} height={480} />
           <S.ProductFooter>
