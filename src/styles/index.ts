adac2d904f910ad6a387153197233340b9a26b80
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-10-23 06:38:04 -0300

    adicionei tamanhos padrões de fontes da aplicação

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -22,5 +22,12 @@ export const {
       green: '#00875F',
       greenLight: '#00B37E',
     },
+
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
   },
 })
