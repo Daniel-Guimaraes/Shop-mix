f6766d5d6d496fb741dfafaa16e11a0d6343a34b
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-10-24 07:39:15 -0300

    Criei a estrutura html da página de detalhes do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,23 @@
+import * as S from '@/styles/pages/product'
+
 export default function ProductDetails() {
-  return <h1>Product Details</h1>
+  return (
+    <S.ProductContainer>
+      <S.ImageContainer></S.ImageContainer>
+
+      <S.ProductDetails>
+        <h1>Produto X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. A enim,
+          numquam deleniti, obcaecati laborum nam illum quisquam quibusdam eum
+          aspernatur dolore sit excepturi, modi voluptates tempore. Cum unde
+          eaque porro.
+        </p>
+
+        <button>Comprar agora</button>
+      </S.ProductDetails>
+    </S.ProductContainer>
+  )
 }
