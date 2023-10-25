59340a9281b13cce672a4aee6acf1f8dd7fb0d4d
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-10-25 20:54:07 -0300

    adicionado condicionais para a rota de checkout, e mudanças na url de destino

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,9 +5,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const priceId = 'price_1O4LqCEiAq8HYAZNyl8otDLi'
+  const { priceId } = req.body
 
-  const successUrl = `${process.env.URL_HTTP}/success`
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' })
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found' })
+  }
+
+  const successUrl = `${process.env.URL_HTTP}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.URL_HTTP}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
